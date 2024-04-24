c582ec59e34bedf43e7087b3f548255e1260c154
Vilberto02 <albertojulcapatricio@gmail.com>
2024-04-24 18:28:40 -0500

    cambios

---
+++ Soluciones/Java/RegistroEmpleadosV1/src/clases/Nodo.java
@@ -0,0 +1,15 @@
+
+package clases;
+
+public class Nodo {
+    public String codigo;
+    public String nombre;
+    public String sueldo;
+    public Nodo sig;
+    
+    public Nodo(String cod, String nom, String suel){
+        codigo = cod;
+        nombre = nom;
+        sueldo = suel;
+    }
+}
